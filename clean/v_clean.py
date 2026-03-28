a96d1f7d6b015a78aabed8983cfdb1abc9c47889
Veljko Jovanović <veljaapolon@gmail.com>
2026-03-28 16:35:43 +0100

    Cleaning added

--- clean/v_clean.py
+++ clean/v_clean.py
@@ -13,5 +13,25 @@ def dummy_clean(df : pd.DataFrame) -> pd.DataFrame:
     """
         odvoji po kolonama ove dummy funk...
     """
+    df = df.copy()
+
+    subset_without_first = df.columns[1:]
+
+    df = df.drop_duplicates(subset=subset_without_first, keep='first')
+    ###############################################################################################
+    num_last_cols = 22
+    last22_cols = df.columns[-num_last_cols:]
+    other_cols = df.columns[1:-num_last_cols] 
+
+    df['group_id'] = pd.factorize(
+        df[other_cols].fillna('NaN').astype(str).agg('-'.join, axis=1)
+    )[0]
+
+    df[last22_cols] = df.groupby('group_id')[last22_cols].transform('mean')
+
+
+    df_unique = df.drop_duplicates(subset=other_cols, keep='first').copy()
+
+    df_unique = df_unique.drop(columns=['group_id'])
     
     return df
